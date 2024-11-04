f2db0ac2aec1eb435e151d3f4fed36c7d6a85e75
Esteban <vivaheredia2535@outlook.com>
2024-11-03 19:52:34 -0600

    first commifaft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIf, NgFor } from '@angular/common';
 
+import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
